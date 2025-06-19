aa483c1d1c40c888e1287e5317e008a98ac1f17f
DELL 5290 <DELL 5290@MR-ABHISEK>
2025-06-19 15:41:48 +0530

    Add project files.

---
+++ AspNetCoreCrudWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AspNetCoreCrudWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreCrudWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ AspNetCoreCrudWeb/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreCrudWeb.Models
+{
+    public class Student
+    {
+        [Key]
+        [Display(Name="Student ID")]
+
+        public int StudentId { get; set; }
+
+        [Required]
+        [Display(Name="Student Name")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Course { get; set; }
+
+        [Required]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
